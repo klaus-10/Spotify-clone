788d5db128dfe088b39156aaf54d90db56a08395
klaus-10 <k.shala@campus.unimib.it>
2023-02-09 10:28:18 +0100

    player-trouble

--- src/components/music-player/MusicPlayer.js
+++ src/components/music-player/MusicPlayer.js
@@ -1,74 +1,82 @@
-import React, { useRef, useState, useEffect } from "react";
-import WaveSurfer from "wavesurfer.js";
-import axios from "axios";
+// import React, { useRef, useState, useEffect } from "react";
+// import WaveSurfer from "wavesurfer.js";
+// import axios from "axios";
 
-import testMp3 from "../../assets/audio/Arai.mp3";
+// import testMp3 from "../../assets/audio/Arai.mp3";
+
+// const MusicPlayer = () => {
+//   const [isPlaying, setIsPlaying] = useState(false);
+//   const containerRef = useRef(null);
+//   let wavesurfer = null;
+
+//   const ctx = new AudioContext();
+
+//   useEffect(() => {
+//     wavesurfer = WaveSurfer.create({
+//       container: containerRef.current,
+//       backend: "MediaElement",
+//       mediaControls: true,
+//       waveColor: "violet",
+//       progressColor: "purple",
+//       barWidth: 2,
+//       barHeight: 1,
+//       barGap: null,
+//       xhr: {
+//         cache: "default",
+//         mode: "no-cors",
+//         method: "GET",
+//         credentials: "same-origin",
+//         redirect: "follow",
+//         referrer: "client",
+//         headers: [{ key: "Authorization" }],
+//       },
+//     });
+
+//     wavesurfer.load(
+//       // mp3
+//       "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/319/insides-feat-jessy-kvge-1675818045-O8B8m9trDG.mp3"
+//     );
+//   }, []);
+
+//   const togglePlay = () => {
+//     if (isPlaying) {
+//       wavesurfer.pause();
+//     } else {
+//       wavesurfer.play();
+//     }
+//     setIsPlaying(!isPlaying);
+//   };
+
+//   return (
+//     <div>
+//       <div ref={containerRef} />
+//       <button onClick={togglePlay}>{isPlaying ? "Pause" : "Play"}</button>
+//     </div>
+//   );
+// };
+
+// export default MusicPlayer;
+
+import React, { useState, useEffect } from "react";
 
 const MusicPlayer = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const containerRef = useRef(null);
-  let wavesurfer = null;
-
-  const ctx = new AudioContext();
+  const [audioData, setAudioData] = useState(null);
 
   useEffect(() => {
-    wavesurfer = WaveSurfer.create({
-      container: containerRef.current,
-      backend: "MediaElement",
-      mediaControls: true,
-      waveColor: "violet",
-      progressColor: "purple",
-      barWidth: 2,
-      barHeight: 1,
-      barGap: null,
-      xhr: {
-        cache: "default",
-        mode: "no-cors",
-        method: "GET",
-        credentials: "same-origin",
-        redirect: "follow",
-        referrer: "client",
-        headers: [{ key: "Authorization" }],
-      },
-    });
-
-    // fetch(
-    //   "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/319/insides-feat-jessy-kvge-1675818045-O8B8m9trDG.mp3",
-    //   {
-    //     mode: "no-cors",
-    //   }
-    // )
-    //   .then((response) => response.arrayBuffer())
-    //   .then((arrayBuffer) => {
-    //     // ctx.decodeAudioData(arrayBuffer))
-    //     // .then((decodeAudio) => {
-    //     // audio = decodeAudio;
-    //     wavesurfer.load(
-    //       arrayBuffer
-    //       // "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/319/insides-feat-jessy-kvge-1675818045-O8B8m9trDG.mp3"
-    //     );
-    //   })
-    //   .catch((e) => console.error(e));
-
-    wavesurfer.load(
-      // mp3
+    fetch(
       "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/319/insides-feat-jessy-kvge-1675818045-O8B8m9trDG.mp3"
-    );
+    )
+      .then((response) => response.arrayBuffer())
+      .then((arrayBuffer) => setAudioData(arrayBuffer));
   }, []);
-
-  const togglePlay = () => {
-    if (isPlaying) {
-      wavesurfer.pause();
-    } else {
-      wavesurfer.play();
-    }
-    setIsPlaying(!isPlaying);
-  };
 
   return (
     <div>
-      <div ref={containerRef} />
-      <button onClick={togglePlay}>{isPlaying ? "Pause" : "Play"}</button>
+      {audioData ? (
+        <p>Audio data successfully fetched and saved in the variable.</p>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
