6cb06957d671bc0591814bfb13ba0387461024e9
klaus-10 <k.shala@campus.unimib.it>
2023-02-26 14:40:32 +0100

    Radio & Concert page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MusicPlayer from "./components/music-player/MusicPlayer";
+import Concert from "./views/concert/Concert";
 import Dashboard from "./views/dashboard/Dashboard";
 import Home from "./views/home/Home";
 import Library from "./views/library/Library";
 import Log from "./views/login/Log";
 import Playlist from "./views/playlist/Playlist";
+import Radio from "./views/radio/Radio";
 import Explore from "./views/search/Explore";
 import Track from "./views/track/Track";
 
@@ -24,6 +26,8 @@ function App() {
             <Route path="library" element={<Library />} />
             <Route path="genre/:id" element={<Home />} />
             <Route path="album/:id" element={<Home />} />
+            <Route path="radio" element={<Radio />} />
+            <Route path="concert" element={<Concert />} />
           </Route>
 
           <Route path="/log" element={<Log />} />
---
+++ src/views/radio/Radio.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Radio() {
+  return (
+    <div className="flex-center" style={{ height: "calc(100vh - 70px)" }}>
+      <h1>Comming soon...</h1>
+    </div>
+  );
+}
